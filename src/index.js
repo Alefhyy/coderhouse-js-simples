6d288c12ce6635bf13ea6b1c930a38cfbb3b3424
Alefh <92604413+Alefhyy@users.noreply.github.com>
2022-11-03 18:39:17 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,19 @@
+     const nome = prompt('Qual é o seu nome?')
+
+    if (nome == "") {
+        alert ('Precisa colocar seu nome!')
+    } else if(nome == "bruno") {
+        alert('Olá professor ' + nome + '! ' + 'Bem vindo ao meu site' + '!')
+    } else if(nome == "Bruno") {
+        alert('Olá professor ' + nome + '! ' + 'Bem vindo ao meu site' + '!')
+    } else if(nome == "andre") {
+        alert('Olá tutor ' + nome + '! ' + 'Bem vindo ao meu site' + '!')
+    } else if(nome == "Andre") {
+        alert('Olá tutor ' + nome + '! ' + 'Bem vindo ao meu site' + '!')
+    } else if(nome == "nathalia") {
+        alert('Olá tutor ' + nome + '! ' + 'Bem vindo ao meu site' + '!')
+    } else if(nome == "Nathalia") {
+        alert('Olá tutor ' + nome + '! ' + 'Bem vindo ao meu site' + '!')
+    } else {
+        alert('Olá ' + nome + '! ' + 'Bem vindo ao meu site' + '!')
+    }
